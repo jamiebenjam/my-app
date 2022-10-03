9850f3c59dd9cd0c187924f4e39291bf78c05ffc
Jamie Benjamin <bennythedev@gmail.com>
2022-10-03 16:44:50 -0500

    Updates

---
+++ precache-manifest.cffc8dc6eaf299387977c58bb4c8d2ba.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "2efc7b9bde19302e4678",
+    "url": "/portfolio-site/static/css/main.e4a98b21.chunk.css"
+  },
+  {
+    "revision": "2efc7b9bde19302e4678",
+    "url": "/portfolio-site/static/js/main.d564b79e.chunk.js"
+  },
+  {
+    "revision": "4a1f3ffe683d329712e7",
+    "url": "/portfolio-site/static/js/runtime~main.7097ca48.js"
+  },
+  {
+    "revision": "f289c55b35305e54d169",
+    "url": "/portfolio-site/static/css/2.58bf5a53.chunk.css"
+  },
+  {
+    "revision": "f289c55b35305e54d169",
+    "url": "/portfolio-site/static/js/2.db658ef7.chunk.js"
+  },
+  {
+    "revision": "242f9cb65021bb3dbf9b",
+    "url": "/portfolio-site/static/js/3.5b5b5cc8.chunk.js"
+  },
+  {
+    "revision": "c4338691f6ef7e4fa43583466f7aa2ed",
+    "url": "/portfolio-site/static/media/dive.c4338691.png"
+  },
+  {
+    "revision": "7a042b5eb1be1ec995732eaeaa8b68fe",
+    "url": "/portfolio-site/static/media/ecom2.7a042b5e.png"
+  },
+  {
+    "revision": "c9b8817f5c9fe887831cdff8b4b6b295",
+    "url": "/portfolio-site/static/media/ecom3.c9b8817f.png"
+  },
+  {
+    "revision": "6462cd762b6bdcc1bacc2fd6f61ee025",
+    "url": "/portfolio-site/static/media/ranch.6462cd76.png"
+  },
+  {
+    "revision": "28ce13c957426c73db81e575739e215a",
+    "url": "/portfolio-site/static/media/ranch2.28ce13c9.png"
+  },
+  {
+    "revision": "214504cff00bbcb2cf4d488507141555",
+    "url": "/portfolio-site/static/media/ranch3.214504cf.png"
+  },
+  {
+    "revision": "dcecb0e664e560a6bca7fa77d8924630",
+    "url": "/portfolio-site/static/media/ecommerce.dcecb0e6.png"
+  },
+  {
+    "revision": "d110fcfeebe2bd23acd549a497e3a78f",
+    "url": "/portfolio-site/static/media/Jamie_Benjamin_Resume.d110fcfe.pdf"
+  },
+  {
+    "revision": "17fe0f931410204d383586cb124429c7",
+    "url": "/portfolio-site/static/media/me.17fe0f93.jpg"
+  },
+  {
+    "revision": "6e7c0fbeb880bebe6bb9a42211394b6e",
+    "url": "/portfolio-site/static/media/argestadisplay-regular-webfont.6e7c0fbe.ttf"
+  },
+  {
+    "revision": "36914708adb32dd10132b311ba7d896a",
+    "url": "/portfolio-site/static/media/North Carossela.36914708.otf"
+  },
+  {
+    "revision": "e3771b16594c52ebb3e5877bcf338196",
+    "url": "/portfolio-site/static/media/MADEOkineSansPERSONALUSE-Medium.e3771b16.otf"
+  },
+  {
+    "revision": "18ce37a110f972cae3849425a6f3430f",
+    "url": "/portfolio-site/static/media/Sekogure.18ce37a1.ttf"
+  },
+  {
+    "revision": "651b51cc4409a3926f85aa25861852ce",
+    "url": "/portfolio-site/static/media/coolvetica rg.651b51cc.otf"
+  },
+  {
+    "revision": "7f5142ad3a1ba0f226eeb9a57cf8fe77",
+    "url": "/portfolio-site/static/media/Avigea.7f5142ad.ttf"
+  },
+  {
+    "revision": "61072ed99653a07fa792aa5f2a75cae8",
+    "url": "/portfolio-site/static/media/StretchPro.61072ed9.otf"
+  },
+  {
+    "revision": "25a4be2eb73df348089f40006996b06e",
+    "url": "/portfolio-site/static/media/by-the-pool-14.25a4be2e.jpeg"
+  },
+  {
+    "revision": "6111e28afb63fb5bfcdcdac69517d125",
+    "url": "/portfolio-site/static/media/ecom1.6111e28a.png"
+  },
+  {
+    "revision": "7869003d3a099a5ea5e07e8dc0799db3",
+    "url": "/portfolio-site/static/media/ranch1.7869003d.png"
+  },
+  {
+    "revision": "2a64d181417a3736800ff81c3aee2059",
+    "url": "/portfolio-site/index.html"
+  }
+];
